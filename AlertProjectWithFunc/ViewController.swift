03a56c2f0b2b1470bfd5b8325859509fa734b81d
emineaaaa <emine0aydinli3@gmail.com>
2023-08-04 19:44:47 +0300

    Initial Commit

---
+++ AlertProjectWithFunc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertProjectWithFunc
+//
+//  Created by Emine Aydınlı on 4.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
